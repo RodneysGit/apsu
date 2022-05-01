63a2016f038497a32cdde5eb802fbdf3aef147f1
DevComputer <DevComputer@DESKTOP-H3OK7QS>
2022-05-01 22:45:07 +0200

    'apSU'

---
+++ git_push.bat
@@ -0,0 +1,6 @@
+git init
+git add -A
+git commit -m 'apSU'
+git remote add origin git@github.com:RodneysGit/apSU.git
+git push -u -f origin master
+@pause
--- old/run.bat
+++
@@ -1,1 +0,0 @@
-attrib -s -h C:\Users\Rodney1\Downloads\Downloads\New_folder_(2)
